5b05413f6732b10ea2f17e88ccdcdcc0692e706e
Corgano <Corganowade@gmail.com>
2026-09-12 01:01:24 -0600

    Hide debug controls behind console command

--- src/js/05-boot.js
+++ src/js/05-boot.js
@@ -1,4 +1,31 @@
+window.__arenaDebugEnabled = false;
+DEBUG_LEVEL = 0;
+
+function updateDebugControlsVisibility() {
+      const controls = document.querySelector(".debug-controls");
+      if (controls) {
+        controls.style.display = window.__arenaDebugEnabled ? "flex" : "none";
+      }
+    }
+
+window.enableDebug = function() {
+      window.__arenaDebugEnabled = true;
+      DEBUG_LEVEL = 1;
+      updateDebugControlsVisibility();
+      console.info("[ARENA DEBUG] Debug options enabled. Use the verbosity buttons to change logging level.");
+};
+
+window.disableDebug = function() {
+      window.__arenaDebugEnabled = false;
+      DEBUG_LEVEL = 0;
+      updateDebugControlsVisibility();
+      console.info("[ARENA DEBUG] Debug options disabled.");
+};
+
+console.info("[ARENA] Debug options are hidden. Run enableDebug() in the console to reveal them.");
+
 function bootGame() {
+      updateDebugControlsVisibility();
       const canvasElement = document.getElementById("gameCanvas");
       if (canvasElement) {
         const game = new GameManager(canvasElement);
