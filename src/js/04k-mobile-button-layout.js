cc1f18a56310dc061ff37eabda6680062447ea67
Corgano <Corganowade@gmail.com>
2026-09-13 15:39:37 -0600

    Align landscape mobile utility buttons with movement control

--- src/js/04k-mobile-button-layout.js
+++ src/js/04k-mobile-button-layout.js
@@ -6,21 +6,29 @@
 const balttatoMobileButtonLayoutStyle = document.createElement("style");
 balttatoMobileButtonLayoutStyle.textContent = `
   body.balttato-landscape-mobile:not(.balttato-controls-swapped) #mobile-fullscreen-button {
-    left: calc(100% - 134px);
+    left: calc(100% - 132px);
+    top: 58px;
+    bottom: auto;
   }
 
   body.balttato-landscape-mobile:not(.balttato-controls-swapped) #mobile-swap-button {
-    left: calc(100% - 74px);
+    left: calc(100% - 72px);
+    top: 58px;
+    bottom: auto;
   }
 
   body.balttato-landscape-mobile.balttato-controls-swapped #mobile-swap-button {
-    left: 74px;
+    left: 18px;
+    top: 58px;
+    bottom: auto;
   }
 
   body.balttato-landscape-mobile.balttato-controls-swapped #mobile-fullscreen-button {
-    left: 134px;
+    left: 78px;
+    top: 58px;
+    bottom: auto;
   }
 `;
 document.head.appendChild(balttatoMobileButtonLayoutStyle);
 
-logDebug(1, "Landscape mobile utility buttons positioned side-by-side");
+logDebug(1, "Landscape mobile utility buttons positioned beside movement control");
